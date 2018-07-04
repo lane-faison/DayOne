6394e0c353efc56a976a7db1766e1c7d66d7f2c1
Lane Faison <lane.faison@icloud.com>
2018-07-04 11:12:20 -0600

    UI work

--- DayOne/View Controllers/CreateJournalViewController.swift
+++ DayOne/View Controllers/CreateJournalViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
 
 class CreateJournalViewController: UIViewController {
-
+    
+    let blueTheme = UIColor(red: 0.298, green: 0.757, blue: 0.988, alpha: 1.0) // #4CC1FC
+    
+    @IBOutlet weak var statusBarView: UIView!
     @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var journalTextView: UITextView!
@@ -12,6 +15,10 @@ class CreateJournalViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupView()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: Notification.Name.UIKeyboardWillShow, object: nil)
     }
     
     @IBAction func cancelTapped(_ sender: Any) {
@@ -27,5 +34,29 @@ class CreateJournalViewController: UIViewController {
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
+    }
+}
+
+extension CreateJournalViewController {
+    private func setupView() {
+        statusBarView.backgroundColor = blueTheme
+        navBar.barTintColor = blueTheme
+        navBar.tintColor = .white
+        navBar.isTranslucent = false
+        navBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    
+    private func changeKeyboardHeight(notification: Notification) {
+        if let keyboardFrame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyHeight = keyboardFrame.cgRectValue.height
+        }
+    }
+    
+    @objc private func keyboardWillHide() {
+        
+    }
+    
+    @objc private func keyboardWillShow() {
+        
     }
 }
