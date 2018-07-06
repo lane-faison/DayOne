a9f85e4cdac6948b56429da9bf1f2073df863a00
Lane Faison <lane.faison@icloud.com>
2018-07-06 13:19:08 -0600

    ImagePicker updates

--- DayOne/View Controllers/CreateJournalViewController.swift
+++ DayOne/View Controllers/CreateJournalViewController.swift
@@ -2,8 +2,6 @@ import UIKit
 
 class CreateJournalViewController: UIViewController {
     
-    let blueTheme = UIColor(red: 0.298, green: 0.757, blue: 0.988, alpha: 1.0) // #4CC1FC
-    
     @IBOutlet weak var statusBarView: UIView!
     @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
@@ -12,8 +10,16 @@ class CreateJournalViewController: UIViewController {
     @IBOutlet weak var setDateButton: UIButton!
     @IBOutlet weak var stackView: UIStackView!
     
+    let blueTheme = UIColor(red: 0.298, green: 0.757, blue: 0.988, alpha: 1.0) // #4CC1FC
+    
+    var date = Date()
+    var imagePicker = UIImagePickerController()
+    var images: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
         
         setupView()
         
@@ -24,7 +30,7 @@ class CreateJournalViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        navBar.topItem?.title = "July 6"
+        updateDate()
     }
     
     @IBAction func cancelTapped(_ sender: Any) {
@@ -34,12 +40,23 @@ class CreateJournalViewController: UIViewController {
     }
     
     @IBAction func setDateTapped(_ sender: Any) {
+        journalTextView.isHidden = false
+        datePicker.isHidden = true
+        setDateButton.isHidden = true
+        date = datePicker.date
+        updateDate()
     }
     
     @IBAction func calendarTapped(_ sender: Any) {
+        journalTextView.isHidden = true
+        datePicker.isHidden = false
+        setDateButton.isHidden = false
+        datePicker.date = date
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
 }
 
@@ -50,6 +67,12 @@ extension CreateJournalViewController {
         navBar.tintColor = .white
         navBar.isTranslucent = false
         navBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    
+    private func updateDate() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, MMM, d, yyyy"
+        navBar.topItem?.title = dateFormatter.string(from: date)
     }
     
     private func changeKeyboardHeight(notification: Notification) {
@@ -66,5 +89,28 @@ extension CreateJournalViewController {
     
     @objc private func keyboardWillShow(notification: Notification) {
         changeKeyboardHeight(notification: notification)
+    }
+}
+
+extension CreateJournalViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            images.append(chosenImage)
+            // Animation
+            let imageView = UIImageView()
+            
+            NSLayoutConstraint.activate([
+                imageView.heightAnchor.constraint(equalToConstant: 70.0),
+                imageView.widthAnchor.constraint(equalToConstant: 70.0)
+                ])
+            
+            imageView.image = chosenImage
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+            stackView.addArrangedSubview(imageView)
+            imagePicker.dismiss(animated: true) {
+                // Animation
+            }
+        }
     }
 }
